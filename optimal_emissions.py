37621f89ae0910d793c23b47c375271f634d35da
KrishM123 <kahnscatterkrish@gmail.com>
2024-06-23 03:24:04 -0700

    finished function

--- optimal_emissions.py
+++ optimal_emissions.py
@@ -2,6 +2,7 @@ import pandas as pd
 import json
 import math
 import numpy as np
+import sys
 
 
 emission_decay = lambda year: math.e ** (-0.02 * year)
@@ -15,9 +16,9 @@ def country_to_code(country):
 def find_optimal_split(fs, gs, country_code, period):
     min_split = 0
     max_corr = 0
-    for split in range(period):
-        if abs(fs[str(split)][country_code][1]) + abs(gs[str(period - split)][country_code][1]) > max_corr:
-            max_corr = abs(fs[str(split)][country_code][1]) + abs(gs[str(period - split)][country_code][1])
+    for split in range(1, period):
+        if abs(fs[country_code][str(split)][1]) + abs(gs[country_code][str(period - split)][1]) > max_corr:
+            max_corr = abs(fs[country_code][str(split)][1]) + abs(gs[country_code][str(period - split)][1])
             min_split = split
     return min_split
 
@@ -38,17 +39,19 @@ def get_emissions_graph(country, period, similar_country):
         gs = json.load(f)
     similar_country_code = country_to_code(similar_country)
     country_code = country_to_code(country)
-    emission = pd.read_csv('emission.csv').drop('CO2 Emissions Per Capita', axis=1).drop('Entity', axis=1)
-    economy = pd.read_csv('economy.csv').drop('GDP Per Capita', axis=1).drop('Entity', axis=1)
-    current_emission = emission[emission['Code'] == country_code][2019]
-    current_economy = economy[economy['Code'] == country_code][2019]
 
-    minimum_total_emission = max(int)  # set to max int
-    for emission_to_test in range(current_emission * 0.8, current_emission * 1.2, current_emission * 0.1):
+    economy_dict = pd.read_csv('economy.csv').drop('Entity', axis=1).groupby('Code').apply(lambda x: x.set_index('Year')['GDP Per Capita'].to_dict()).to_dict()
+    emission_dict = pd.read_csv('emission.csv').drop('Entity', axis=1).groupby('Code').apply(lambda x: x.set_index('Year')['CO2 Emissions Per Capita'].to_dict()).to_dict()
+
+    current_emission = emission_dict[country_code][2019]
+    current_economy = economy_dict[country_code][2019]
+
+    minimum_total_emission = sys.maxsize  # set to max int
+    for emission_to_test in range(int(current_emission * 0.5), int(current_emission * 1.5), max(int(current_emission * 0.1), 1)):
         total_emission = 0
         for delta in range(2, period, 2):
             split = find_optimal_split(fs, gs, similar_country_code, delta)
-            total_emission += current_emission * np.poly1d(gs[similar_country_code][str(delta - split)][0])(current_economy * np.poly1d(fs[similar_country_code][str(split)][0])(emission_to_test)) * emission_decay(delta)
+            total_emission += abs(current_emission * np.poly1d(gs[similar_country_code][str(delta - split)][0])(current_economy * np.poly1d(fs[similar_country_code][str(split)][0])(emission_to_test)) * emission_decay(delta))
         if total_emission < minimum_total_emission:
             minimum_total_emission = total_emission
             optimal_emission = emission_to_test
@@ -56,6 +59,5 @@ def get_emissions_graph(country, period, similar_country):
     future_emissions = []
     for delta in range(2, period, 2):
         split = find_optimal_split(fs, gs, similar_country_code, delta)
-        future_emissions.append(current_emission * np.poly1d(gs[similar_country_code][str(delta - split)][0])(current_economy * np.poly1d(fs[similar_country_code][str(split)][0])(optimal_emission)) * emission_decay(delta))
-    
+        future_emissions.append(abs(np.poly1d(fs['WRLD'])(delta) * (optimal_emission)))
     return future_emissions
