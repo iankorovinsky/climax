0beb7e64b3cc107436491986a367def5506dd6a7
Simerus Mahesh <63352151+SimerusM@users.noreply.github.com>
2024-06-23 04:41:39 -0700

    added graph for predicted values

--- app/src/components/MiniModal.js
+++ app/src/components/MiniModal.js
@@ -1,8 +1,64 @@
-// src/MiniModal.js
 import React from 'react';
 import Draggable from 'react-draggable';
+import { Line } from 'react-chartjs-2';
 
-const MiniModal = ({ header,content, position, onClose }) => {
+const MiniModal = ({ header, content, position, onClose }) => {
+  const parseData = (data) => {
+    try {
+      return JSON.parse(data);
+    } catch (error) {
+      console.error('Failed to parse data:', error);
+      return null;
+    }
+  };
+
+  const renderContent = (header, content) => {
+    if (header === "Calculate Prediction") {
+      const parsedData = parseData(content);
+      if (parsedData) {
+        const labels = Object.keys(parsedData.prediction);
+        const dataValues = Object.values(parsedData.prediction);
+
+        const data = {
+          labels: labels,
+          datasets: [
+            {
+              label: 'Predicted Values',
+              data: dataValues,
+              borderColor: 'rgba(75,192,192,1)',
+              backgroundColor: 'rgba(75,192,192,0.2)',
+            },
+          ],
+        };
+
+        const options = {
+          plugins: {
+            legend: {
+              labels: {
+                color: 'white' // Change the color here
+              }
+            }
+          },
+          scales: {
+            x: {
+              ticks: {
+                color: 'white' // Change the color here
+              }
+            },
+            y: {
+              ticks: {
+                color: 'white' // Change the color here
+              }
+            }
+          }
+        };
+
+        return <Line data={data} options={options} />;
+      }
+    } else {
+      return <p style={{ color: "white" }}>{content}</p>;
+    }
+  };
 
   return (
     <Draggable>
@@ -11,9 +67,8 @@ const MiniModal = ({ header,content, position, onClose }) => {
           <div>
             <button className="close-button" onClick={onClose}>🌎</button>
           </div>
-        
-          <h2>{header}</h2>
-          <p>{content}</p>
+          <h2 style={{ color: "white" }}>{header}</h2>
+          {renderContent(header, content)}
         </div>
       </div>
     </Draggable>
